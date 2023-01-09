821e01f5503c26def9c51b0f61315c0e5040fe6e
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-09 14:49:45 +0100

    aggiunge metodo per il rating

--- classes/movie.php
+++ classes/movie.php
@@ -5,13 +5,22 @@ class Movie {
     public $rating;
     public $genere;
     public $language;
-    function __construct( string $_name , int $_rating , string $_genere , string $_language)
+    public $plot;
+    function __construct( string $_name , int $_rating , string $_genere , string $_language , string $_plot)
     {
         $this ->name = $_name;
         $this ->rating = $_rating;
         $this ->genere = $_genere;
-        $this ->language = $_language;   
+        $this ->language = $_language;
+        $this->setRating($_plot);   
     }
+
+    function setRating($_plot){
+        if($this->rating > 6){
+            $this->plot = $_plot;
+        }
+    }
+
 }
     
 
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__.'/classes/movie.php';
 
-$ritornoalfuturo = new Movie("back to the future" , 8 , "Fantascienza" , "eng");
-$ilbuonoilbruttoilcattivo = new Movie("il buono , il brutto , il cattivo", 7 , "western" , "eng");
+$ritornoalfuturo = new Movie("back to the future" , 8 , "Fantascienza" , "eng" , "Il diciottenne Marty ha come amico un bislacco scienziato che ha inventato una macchina per viaggiare nel tempo. Grazie ad essa Marty tornerà indietro nel tempo, fino al 1955.");
+$ilbuonoilbruttoilcattivo = new Movie("il buono , il brutto , il cattivo", 5 , "western" , "eng" , "Mentre infuria la Guerra di Secessione, tre uomini dall'oscuro passato si battono per impossessarsi di un tesoro nascosto in un cimitero.");
 var_dump($ritornoalfuturo , $ilbuonoilbruttoilcattivo);
